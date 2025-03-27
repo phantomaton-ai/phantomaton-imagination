79194225eb3c14a24f09d85588c55aca97cc0ab8
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-27 14:49:38 -0700

    Updated by Phantomaton

--- phantomaton-imagination.js
+++ phantomaton-imagination.js
@@ -1,9 +1,13 @@
 import plugins from 'phantomaton-plugins';
+import execution from 'phantomaton-execution';
+import commands from './commands.js';
 
 const api = plugins.create({
   generateImage: plugins.singleton,
   imageConfig: plugins.optional,
   imageProvider: plugins.composite
-});
+}, ({configuration, extensions}) => [
+  plugins.define(execution.command).as(commands(configuration))
+]);
 
 export default api;
